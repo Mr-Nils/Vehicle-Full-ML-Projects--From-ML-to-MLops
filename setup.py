04db63827bc7f98243ade77d6d7bc1bf2c391f4d
Mr-Nils <nilkonthadas16@gmail.com>
2026-06-26 12:39:07 +0600

    Project Structure Setup Ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Nilkontha Das",
+    author_email="nilkonthadas16@gmail.com",
+    packages=find_packages()
+)
